bc6351b7cf263a5e64cd09790f2349c5b93a2f19
Sipos Miklos <commit@siposm.hu>
2025-05-02 10:14:27 +0200

    service

--- angular/devcrud/src/app/stats/stats.component.ts
+++ angular/devcrud/src/app/stats/stats.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Developer } from '../developer';
+import { DeveloperService } from '../developer.service';
+import { StatisticsService } from '../statistics.service';
 
 @Component({
   selector: 'app-stats',
@@ -8,40 +10,5 @@ import { Developer } from '../developer';
   styleUrl: './stats.component.sass'
 })
 export class StatsComponent {
-  developers: Developer[] = []
-
-  constructor() {
-    this.load()
-  }
-
-  load(): void {
-    let data = JSON.parse(localStorage.getItem("bprof_devs") ?? "[]")
-    Object.values(data).map(x => this.developers.push(Object.assign(new Developer(), x)))
-  }
-
-  averageSalary(): number {
-    let sum = this.developers.map(x => x.salary).reduce((a, b) => a! + b!)!
-    let avgSalary = Math.round(sum! / this.developers.length)
-    return avgSalary
-  }
-
-  oldestDeveloper(): Developer {
-    return this.developers.reduce((a, b) => a.age! < b.age! ? b : a)
-  }
-
-  highestEarningdeveloper(): Developer {
-    return this.developers.reduce((a, b) => a.salary! < b.salary! ? b : a)
-  }
-
-  lowestEarningDeveloper(): Developer {
-    return this.developers.reduce((a, b) => a.salary! < b.salary! ? a : b)
-  }
-
-  mostSkilledDeveloper(): Developer {
-    return [...this.developers].sort((a, b) => b.skills.length - a.skills.length)[0]
-  }
-
-  getAllSkills(): string[] {
-    return Array.from(new Set(this.developers.map(x => x.skills).flat().sort()))
-  }
+  constructor(public statService: StatisticsService) { }
 }
